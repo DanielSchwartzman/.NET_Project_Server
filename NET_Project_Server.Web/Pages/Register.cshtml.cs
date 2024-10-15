62039c96f1ec686eb2f9455834a13cd82aac3f39
Daniel Schwartzman <Danielshvart@gmail.com>
2024-10-15 12:52:23 +0300

    Added register page, renamed privacy page => About page, improved home page

---
+++ NET_Project_Server.Web/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_Project_Server.Web.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
