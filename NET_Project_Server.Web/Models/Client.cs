8bcc17530fadcb7e0870bb4234e3eefec67314a0
Daniel Schwartzman <Danielshvart@gmail.com>
2024-10-15 14:21:29 +0300

    Added registration form

---
+++ NET_Project_Server.Web/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_Project_Server.Web.Models
+{
+    public class Client
+    {
+        public string? Name { get; set; }
+        public string? ID { get; set; }
+        public string? Phone { get; set; }
+        public string? Country { get; set; }
+    }
+}
--- NET_Project_Server.Web/Pages/Register.cshtml.cs
+++ NET_Project_Server.Web/Pages/Register.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NET_Project_Server.Web.Models;
+using System;
 
 namespace NET_Project_Server.Web.Pages
 {
@@ -7,6 +9,17 @@ namespace NET_Project_Server.Web.Pages
     {
         public void OnGet()
         {
+
+        }
+
+
+        [BindProperty]
+        public Client client { get; set; } = default!;
+
+        public void OnPost()
+        {
+            List<string?> list = new List<string?>() { client.Name, client.ID, client.Phone, client.Country };
+
         }
     }
 }
