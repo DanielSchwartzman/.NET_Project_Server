a926b917ce117ab96fd65c8ed35185d11630f8a8
Tomer258 <tomer25898@gmail.com>
2024-10-29 01:39:55 +0200

    added client controller

---
+++ NET_Project_Server.Web/api/ClientsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NET_Project_Server.Web.Data;
+using NET_Project_Server.Web.Models;
+
+namespace NET_Project_Server.Web.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly NET_Project_ServerWebContext _context;
+
+        public ClientsController(NET_Project_ServerWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClient()
+        {
+            return await _context.Client.ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int? id)
+        {
+            var client = await _context.Client.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(int? id, Client client)
+        {
+            if (id != client.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _context.Client.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.ID }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int? id)
+        {
+            var client = await _context.Client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Client.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int? id)
+        {
+            return _context.Client.Any(e => e.ID == id);
+        }
+    }
+}
